6f2eefa5c85a907e8ca0ceb33414f73bc24eba62
Jian-lin Wu <wgl1242254@gmail.com>
2018-04-27 20:09:58 +0800

    取消使用jquery 改用js原生語法

--- instagram-downloader.js
+++ instagram-downloader.js
@@ -1,28 +1,11 @@
 javascript: (function() {
-    //inject jQuery
-    var jqu = document.createElement('script');
-    jqu.src = "https://code.jquery.com/jquery-latest.min.js";
-    document.getElementsByTagName('head')[0].appendChild(jqu);
+    let url="";
 
-    //wait 500ms for.... jquery loading?
-    setTimeout(function() {
-        let appendTarget="body";
-        let jq = jQuery.noConflict();
+    if(document.querySelector("div[role='dialog']")==null){
+        url=document.querySelector("body main article>div img").getAttribute("src");
+    }else{
+        url=document.querySelector("body div[role='dialog'] article>div img").getAttribute("src");
+    }
 
-        //Start to get target image url
-        let url="";
-        //Check are we in the dialog mode?
-        if(jq("div[role='dialog']").length>0){
-            appendTarget="div[role='dialog']";
-            url = jq("body div[role='dialog'] article>div img").attr("src");
-        }else{
-            url = jq("body main article>div img").attr("src");
-        }
-        let fileName = url.split("/")[url.split("/").length - 1];
-
-        //build <a/> and click it
-        let a = jq("<a>").attr("href", url).attr("download", fileName).attr("target", "_blank").appendTo(appendTarget);
-        a[0].click();
-        a.remove();
-    }, 500);
+    window.open(url);
 })()
