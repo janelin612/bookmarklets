44f0600bdf29f7a1977703b99c95f4d6d3518c08
Tars <wgl1242254@gmail.com>
2019-10-02 21:51:45 +0800

    縮排

--- showroom-m3u8.js
+++ showroom-m3u8.js
@@ -1,15 +1,15 @@
-javascript: (function() {
-    var id=document.querySelector("#this-room-profile").href.match(/\d+/)[0];
+javascript: (function () {
+  var id = document.querySelector("#this-room-profile").href.match(/\d+/)[0];
 
-    fetch(`/api/live/streaming_url?room_id=${id}&ignore_low_stream=1`)
-    .then(r=>{
-        return r.json();
-    }).then(j=>{
-        let url=j.streaming_url_list.find(el=>{
-         return el.type=="hls";
-        }).url;
-        prompt("m3u8",url);
-    }).catch(er=>{
-        alert("something wrong~");
+  fetch(`/api/live/streaming_url?room_id=${id}&ignore_low_stream=1`)
+    .then(r => {
+      return r.json();
+    }).then(j => {
+      let url = j.streaming_url_list.find(el => {
+        return el.type == "hls";
+      }).url;
+      prompt("m3u8", url);
+    }).catch(er => {
+      alert("something wrong~");
     })
 })()
